b90b66f596d098ed5bd5708b9952bca37dea75f0
VaibhavMojidra <vmojidra25@gmail.com>
2023-01-28 04:28:52 +0530

    Assignment Operators

---
+++ README.md
@@ -0,0 +1,4 @@
+# Java Demo Assignment Operators
+
+
+[![Vaibhav Mojidra - output.png](https://raw.githubusercontent.com/VaibhavMojidra/Java---Demo-Assignment-Operators/master/output/output.png "Vaibhav Mojidra")](https://vaibhavmojidra.github.io/site/)
---
+++ src/Demo.java
@@ -0,0 +1,24 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		int n1=1;
+		int n2=1;
+		n2+=n1; //n2=n2+n1
+		System.out.println(n2); 
+		
+		int n3=10;
+		int n4=2;
+		n3-=n4; //n3=n3-n4
+		System.out.println(n3);
+		
+		int n5=3;
+		n5*=2; //n5=n5*2
+		System.out.println(n5);
+		
+		int n6=10;
+		n6/=2; //n6=n6/2
+		System.out.println(n6);
+	}
+
+}
